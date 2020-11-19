ba5702dea28464ab4bef23a012563b4f0ffe9e39
Zhouhaohao <xiaozhouwjsw@163.com>
2020-11-19 21:42:36 +0800

    lala

---
+++ helloworld.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+/*#define M 3
+#define N 20
+void fun(char a[M][N],char *b)
+{
+	int i,j,k=0;
+	for(i=0;i<M;i++)
+	{
+		for(j=0;j<N;j++)
+			if(*(*(a+i)+j)) 
+			{
+				b[k++]=*(*(a+i)+j);
+			}
+			else break;
+			b[k]='\0';
+	}
+}
+int main()
+{
+	char w[M][N]={"AAAAAAAAAAAA","BBBBBBB","CC"},a[100];
+	int i;
+	//void NONO();
+	printf("the string \n");
+	for(i=0;i<M;i++) puts(w[i]);
+	printf("\n");
+	fun(w,a);
+	printf("the A string is: \n");
+	printf("%s\n",a);
+	//NONO();
+	cout<<"hello,c"<<endl;
+	system("pause");
+	return 0;
+}
+
+
+void NONO()
+{
+	char w[M][N],a[100];
+	int i;
+	FILE *rf,*wf;
+	rf=fopen("in.dat","r");
+	wf=fopen("out.dat","w");
+	for(i=0;i<10;i++)
+	{
+		fscanf(rf,"%s %s %s ",w[0],w[1],w[2]);
+		fun(w,a);
+		fprintf(wf,"%s\n",a);
+	}
+	fclose(rf);
+	fclose(wf);
+}*/
+int main()
+{
+	cout << "hello,c";
+	return 0;
+}
